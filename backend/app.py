3b1bf74058efdc93860ef747f46f7e56f58427d9
Anshul Mall <anshulmall06@gmail.com>
2026-08-06 00:20:28 +0530

    Fix FastAPI syntax error

--- backend/app.py
+++ backend/app.py
@@ -6,25 +6,25 @@ from routes.auth import router as auth_router
 from routes.history import router as history_router
 from routes.ai import router as ai_router
 
+
 app = FastAPI(
     title="AI Crop Disease Detection API"
 )
 
+
 app.add_middleware(
-    CORSMiddleware,
-    app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://ai-based-crop-disease-detection-git-main-tbi4.vercel.app",
         "https://ai-based-crop-disease-detection-2xh7jr4m1-tbi4.vercel.app",
         "http://localhost:3000"
     ],
-   
-)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 app.include_router(auth_router)
 app.include_router(predict_router)
 app.include_router(history_router)
