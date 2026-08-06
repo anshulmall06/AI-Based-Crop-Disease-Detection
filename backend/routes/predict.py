309761eb3676492e8f12f8293a9b9263557069a3
Anshul Mall <anshulmall06@gmail.com>
2026-08-06 09:57:06 +0530

    Make prediction endpoint public

--- backend/routes/predict.py
+++ backend/routes/predict.py
@@ -1,6 +1,5 @@
-﻿from fastapi import APIRouter, UploadFile, File, Depends, HTTPException
+﻿from fastapi import APIRouter, UploadFile, File, HTTPException
 from database.database import prediction_collection
-from middleware.auth_middleware import verify_token
 
 from pathlib import Path
 import shutil
@@ -10,27 +9,20 @@ from ml.predictor import predict
 
 router = APIRouter()
 
-# Upload folder
 BASE_DIR = Path(__file__).resolve().parent.parent
 UPLOAD_DIR = BASE_DIR / "uploads"
-
 UPLOAD_DIR.mkdir(exist_ok=True)
 
 
 @router.post("/predict")
-async def predict_disease(
-    file: UploadFile = File(...),
-    user=Depends(verify_token)
-):
+async def predict_disease(file: UploadFile = File(...)):
 
-    # Check file type
     if not file.content_type.startswith("image/"):
         raise HTTPException(
             status_code=400,
             detail="Please upload an image."
         )
 
-    # Save uploaded image
     file_extension = Path(file.filename).suffix
     filename = f"{uuid.uuid4()}{file_extension}"
     file_path = UPLOAD_DIR / filename
@@ -38,7 +30,6 @@ async def predict_disease(
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # Predict using ONNX
     try:
         result = predict(file_path)
 
@@ -51,16 +42,8 @@ async def predict_disease(
             detail=f"Prediction failed: {str(e)}"
         )
 
-    # Save prediction to MongoDB
-    prediction_data = {
-        "email": user["sub"],
+    return {
         "filename": file.filename,
         "disease": disease,
         "confidence": confidence
     }
-
-    inserted = prediction_collection.insert_one(prediction_data)
-
-    prediction_data["_id"] = str(inserted.inserted_id)
-
-    return prediction_data
